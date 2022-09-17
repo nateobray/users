a3246af4efa19bd31ca0cbd030cfe8fa67282641
nateobray <ubuntu@ip-172-31-8-243.us-west-2.compute.internal>
2022-09-17 15:54:12 +0000

    refactor

---
+++ src/User.php
@@ -0,0 +1,41 @@
+<?php
+namespace obray;
+
+use obray\data\DBO;
+use obray\data\types\Boolean;
+use obray\data\types\BooleanTrue;
+use obray\data\types\DateTimeNullable;
+use obray\data\types\Int11UnsignedDefault0;
+use obray\data\types\Password;
+use obray\data\types\PrimaryKey;
+use obray\data\types\TinyInt1UnsignedDefault1;
+use obray\data\types\Varchar64;
+use obray\data\types\Varchar64Nullable;
+
+Class User extends DBO
+{
+    const TABLE = 'Users';
+    
+    public PrimaryKey $col_user_id;
+    public Varchar64Nullable $col_user_first_name;
+    public Varchar64Nullable $col_user_last_name;
+    public Varchar64 $col_user_email;
+    public Password $col_user_password;
+    public TinyInt1UnsignedDefault1 $col_user_permission_level;
+    public BooleanTrue $col_user_is_active;
+    public Boolean $col_user_is_system;
+    public Int11UnsignedDefault0 $col_user_failed_attempts;
+    public DateTimeNullable $col_user_last_login;
+
+    const INDEXES = [
+        ['user_first_name'],
+        ['user_last_name'],
+        ['user_email', 'UNIQUE'],
+        ['user_permission_level'],
+        ['user_is_active'],
+        ['user_is_system'],
+        ['user_password'],
+        ['user_failed_attempts'],
+        ['user_last_login']
+    ];
+}
---
+++ src/UserRoles.php
@@ -0,0 +1,23 @@
+<?php
+namespace obray;
+
+use obray\data\DBO;
+use obray\data\types\Int11Unsigned;
+use obray\data\types\PrimaryKey;
+
+Class UserRoles extends DBO
+{
+    const TABLE = 'UserRoles';
+    protected PrimaryKey $col_user_role_id;
+    protected Int11Unsigned $col_role_id;
+    protected Int11Unsigned $col_user_id;
+
+    const INDEXES = [
+        [['role_id', 'user_id'], 'UNIQUE']
+    ];
+
+    const FOREIGN_KEYS = [
+        ['role_id', 'Roles', 'role_id'],
+        ['user_id', 'Users', 'user_id']
+    ];
+}
